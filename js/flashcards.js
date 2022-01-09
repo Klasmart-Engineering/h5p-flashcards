0ee95bc63b3b7e4dd54f4388326c2268ba700a87
Oliver Tacke <o.tacke@posteo.de>
2022-01-09 11:57:37 +0100

    H5P-386 Fix orientation detection

--- js/flashcards.js
+++ js/flashcards.js
@@ -408,7 +408,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
         },
         {
           onResult: (result) => {
-            if (window.orientation !== 90) {
+            if (!this.isMobileLandscape()) {
               $card.find('.h5p-textinput').val(result.phrases[0]).focus();
             }
           }
@@ -452,7 +452,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
       var userCorrect = isCorrectAnswer(card, userAnswer, that.options.caseSensitive);
       var done = false;
 
-      if (userAnswer === '' && window.orientation !== 90) {
+      if (userAnswer === '' && !this.isMobileLandscape()) {
         $input.focus();
       }
 
@@ -678,7 +678,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
        is running, and the card will be misplaced */
     $card.one('transitionend', function () {
       if ($card.hasClass('h5p-current') && !$card.find('.h5p-textinput')[0].disabled) {
-        if (window.orientation !== 90) {
+        if (!this.isMobileLandscape()) {
           $card.find('.h5p-textinput').focus();
         }
       }
@@ -842,7 +842,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
 
     // Don't resize if trigger was opening/closing virtual keyboard on mobile
     if (
-      window.orientation === 90 &&
+      self.isMobileLandscape() &&
       document.activeElement &&
       document.activeElement.classList.contains('h5p-textinput')
     ) {
@@ -851,7 +851,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
 
     // Change landscape layout on mobile
     if (typeof window.orientation === 'number') {
-      this.$container.toggleClass('h5p-landscape', window.orientation === 90);
+      this.$container.toggleClass('h5p-landscape', self.isMobileLandscape());
     }
 
     this.containerStyle = this.containerStyle || getComputedStyle(this.$container.get(0));
@@ -878,7 +878,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
         );
       }
 
-      if (displayLimits && window.orientation === 90) {
+      if (displayLimits && self.isMobileLandscape()) {
         // Limit card size, 8 and 4 are default margins and paddings
         $(this).css({
           'max-width': (displayLimits.width - 8 * baseFontSize) + 'px',
@@ -917,7 +917,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
 
       // Workaround for very narrow landscape displays
       const $answer = $(this).find('.h5p-answer');
-      if (displayLimits && window.orientation === 90) {
+      if (displayLimits && self.isMobileLandscape()) {
         if ($(this).find('.h5p-foot').width() < $textInput.outerWidth()) {
           let fontSizeEm = 1;
 
@@ -950,7 +950,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
     }
 
     // Reduce font size if mobile landscape
-    if (displayLimits && window.orientation === 90) {
+    if (displayLimits && this.isMobileLandscape()) {
       this.$inner.children('.h5p-card').each(function () {
 
         // Limit card height, 4 and 6 are default margins and paddings
@@ -1112,7 +1112,7 @@ H5P.Flashcards = (function ($, XapiGenerator) {
     let topWindow = this.getTopWindow();
 
     // iOS doesn't change screen dimensions on rotation
-    let screenSize = (this.isIOS() && window.orientation === 90) ?
+    let screenSize = (this.isIOS() && this.isMobileLandscape()) ?
       { height: screen.width, width: screen.height } :
       { height: screen.height, width: screen.width };
 
@@ -1137,6 +1137,53 @@ H5P.Flashcards = (function ($, XapiGenerator) {
       ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform) ||
       (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
     );
+  };
+
+  /**
+   * Determine whether mobile device in landscape orientation.
+   * @return {boolean} True, if mobile device in landscape orientation.
+   */
+  C.prototype.isMobileLandscape = function () {
+    return this.isMobileDevice() && this.getOrientation() === 'landscape';
+  };
+
+  /**
+   * Detect mobile devices (http://detectmobilebrowsers.com/)
+   * @returns {boolean} True if running on a mobile device.
+   */
+  C.prototype.isMobileDevice = function () {
+    let check = false;
+    (function (a) {
+      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+  };
+
+  /**
+   * Get device orientation.
+   * @return {string} 'portrait' or 'landscape'.
+   */
+  C.prototype.getOrientation = function () {
+    if (screen.orientation && screen.orientation.type) {
+      if (screen.orientation.type.includes('portrait')) {
+        return 'portrait';
+      }
+      else if (screen.orientation.type.includes('landscape')) {
+        return 'landscape';
+      }
+    }
+
+    // Unreliable, as not clear what device's natural orientation is
+    if (typeof window.orientation === 'number') {
+      if (window.orientation === 0 || window.orientation === 180) {
+        return 'portrait';
+      }
+      else if (window.orientation === 90 || window.orientation === -90 || window.orientation === 270) {
+        return 'landscape';
+      }
+    }
+
+    return 'landscape'; // Assume default
   };
 
   /** @const {number} Breakpoint for pad height in landscape orientation */
